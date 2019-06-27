57c1265199717e8fbb654ac4a6e5abdb8777bbff
Alyona Omelkina <alyona_om@mail.ru>
2019-06-27 18:42:18 +0300

    Изменил скрипты более современный формат

--- script/script.js
+++ script/script.js
@@ -2,3 +2,10 @@ document.getElementById('github').onclick = function(){
    console.log('Изучил git и github');
    console.log('Изучил git и github');
 };
+
+
+let message = function(){
+  alert('Изучил git и github');
+};
+
+document.getElementById('github').addEventListener('click', message);
