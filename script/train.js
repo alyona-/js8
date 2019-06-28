1a2ba43869672d8d36a66106129527bd6e801584
Alyona Omelkina <alyona_om@mail.ru>
2019-06-28 17:22:05 +0300

    Первая часть домашки

---
+++ script/train.js
@@ -0,0 +1,19 @@
+let str = "teststr";
+
+console.log(str.substring(6));
+console.log(str.substring(9,15));
+console.log(str.substr(2));
+
+
+console.log(str);
+
+let str2 = "Hello World";
+
+
+console.log(str2.indexOf('o'));
+
+console.log(str2.replace('World', 'Friends'));
+
+let str3 = "apple, orangle, lemon";
+
+console.log(str3.split(', '));
