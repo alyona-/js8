c89186ff425fda65bda487deb41ff90635e143d1
Alyona Omelkina <alyona_om@mail.ru>
2019-07-01 13:05:26 +0300

    Вношу правка в ./script/script.js

--- script/script.js
+++ script/script.js
@@ -1,11 +1,30 @@
-document.getElementById('github').onclick = function(){
-   console.log('Изучил git и github');
-   console.log('Изучил git и github');
-};
+//1) Следующим переменным присвоить значения
+let money = 2000000;
+let income ="500000";
+let addExpenses = "30000,500000,600000";
+let deposit =true;
+let mission = 100000000000000;
+let period =1;
 
 
-let message = function(){
-  alert('Изучил git и github');
-};
+//Используя методы и свойства:
+//Вывести в консоль тип данных значений переменных money, income, deposit;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-document.getElementById('github').addEventListener('click', message);
+//Вывести в консоль длину строки income
+console.log(income.length);
+
+//Вывести в консоль “Период (period) месяцев” и “Цель заработать (mission) рублей/долларов/гривен/юани”
+console.log("Период "+period+" месяцев");
+console.log("Цель заработать "+mission+" рублей/долларов/гривен/юани");
+
+//Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль
+console.log(addExpenses.toLowerCase().split(","));
+
+//Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30), вывести в консоль результат и остаток        от деления
+let budgetDay =money/30;
+
+//Результат money/30 хранится в переменной budgetDay
+console.log(budgetDay);
