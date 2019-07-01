dfaadef2c57910a0564e6c38c4e5326b73019e45
Alyona Omelkina <alyona_om@mail.ru>
2019-07-01 13:01:01 +0300

    save fix

--- script/train.js
+++ script/train.js
@@ -5,7 +5,46 @@
 
 'use strict';
 
-let a = 5;
-console.log('a: ', a);
+// let a = 5;
+// console.log('a: ', a);
+// let question = confirm('Тебе есть 18 лет ?');
+// console.log(question);
+// let question2 = prompt('Сколько тебе лет ?');
+// console.log(question2);
+// console.log(typeof question2);
 
-console.log(confirm('Тебе есть 18 лет ?'));
+//Преобразование типов данных
+
+console.log(5 + '5');
+console.log(typeof (5 + '5'));
+
+console.log(5 - '5');
+console.log(5 * '5');
+console.log('js' / '5');
+
+console.log(5 == '5');
+console.log(5 === '5');
+
+if(true) console.log('Истина');
+if(false)console.log('Этот код выполнится');
+
+let n =4 ;
+
+switch (n) {
+    case 3:
+        console.log('Условие 1');
+        break;
+    case 4:
+        console.log('Условие 2');
+        break;
+    case 5:
+        console.log('Условие 3');
+        break;
+    case 6:
+        console.log('Условие 4');
+        break;
+
+}
+
+let result = n === 3 ? 'верно' : 'не верно';
+console.log('result: ',result);
