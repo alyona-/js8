dc19a49b503b5bfb9f7d592336566fd2b3ac9fb3
Alyona Omelkina <alyona_om@mail.ru>
2019-07-17 20:40:18 +0300

    save week1

--- script/w1/w1_1.js
+++ script/w1/w1_1.js
@@ -4,9 +4,12 @@ const appData = {
   budge: money,
   expenses: {},
   asking: function(){
-      const a = prompt("Откуда деньги?");
-      const b = prompt("Сколько денег");
-      appData.expenses[a] = b;
+      for(let i=0; i< 2; i++){
+          const a = prompt("Откуда деньги?");
+          const b = prompt("Сколько денег");
+          appData.expenses[a] = b;
+      }
+
   }
 };
 
