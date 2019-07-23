84bb9c01f0d2a4ca9936b3331b69b9e085c635d2
Alyona Omelkina <alyona_om@mail.ru>
2019-07-23 22:08:14 +0300

    Правки в  10 задание

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,31 +1,36 @@
-function DomElement(selector, height, weight, bg, fontSize) {
+function DomElement(selector, height, width, bg, fontSize) {
     this.selector = selector;
     this.height = height;
-    this.weight = weight;
+    this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
-}
 
-function NewObject(selector, height, weight, bg, fontSize) {
-    DomElement.apply(this, arguments);
     this.elements1 = function () {
         let body = document.querySelector('body');
         let firstLetter = this.selector.slice(0, 1);
         let elemHTML;
         if (firstLetter === ".") {
             elemHTML = document.createElement("div");
+            elemHTML.className = this.selector;
         } else if (firstLetter === "#") {
             elemHTML = document.createElement("p");
+            elemHTML.id = this.selector.slice(1);
         }
-        elemHTML.style.height = height;
-        elemHTML.style.weight = weight;
-        elemHTML.style.backgroundColor = bg;
-        elemHTML.style.fontSize = fontSize;
+
+        elemHTML.style.cssText = "height: " + this.height + ";\
+            width: " + this.width + "; \
+            background-color: " + this.bg + "; \
+            font-size: " + this.fontSize + ";";
         body.appendChild(elemHTML);
     }
+
+}
+
+function NewObject(selector, height, width, bg, fontSize) {
+    DomElement.apply(this, arguments);
 }
 
 NewObject.prototype = Object.create(DomElement.prototype);
 NewObject.prototype.constructor = NewObject;
-let el1 = new NewObject(".blue", "300px", "200px", "blue", "20px");
+let el1 = new NewObject("#blue", "300px", "200px", "blue", "20px");
 el1.elements1();
