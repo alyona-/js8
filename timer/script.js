6ea07afe0af38ca1f899c36ad9cf7590b55feda6
Alyona Omelkina <alyona_om@mail.ru>
2019-07-25 21:53:18 +0300

    Вторая часть 12 задание

--- timer/script.js
+++ timer/script.js
@@ -1,73 +1,53 @@
-
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('DOMContentLoaded', function () {
     'use strict';
-let textCon = document.querySelector('.textResult');
-textCon.textContent ='hello';
-    //Timer
-    function countTimer(newYear, today){
-        let seconds, minutes, hours,day;
+    let textCon = document.querySelector('.textResult');
 
-        function getTimeRemaining(newYear, today){
-            let dateNY =new Date(newYear).getTime(),
-                dateNow =new Date(today).getTime(),
-                timeRemaining = (dateNY - dateNow) /1000;
-            if(timeRemaining<0){
-                seconds=0;
-                minutes=0;
-                hours=0;
-                day=0;
-            }else{
-                seconds=Math.floor(timeRemaining % 60);
-                minutes=Math.floor((timeRemaining / 60)%60);
-                hours=Math.floor(timeRemaining / 60/60);
-                day=Math.floor(timeRemaining / 60/60/24);
-            }
+    function countTimer(newYear, today) {
+        let day;
 
-            return {timeRemaining,hours,minutes,seconds};
+        function getCountDay(newYear, today) {
+            let dateNY = newYear.getTime(),
+                dateNow = today.getTime(),
+                timeRemaining = (dateNY - dateNow) / 1000;
+            if (timeRemaining < 0)
+                day = 0;
+            else
+                day = Math.floor(timeRemaining / 60 / 60 / 24);
+            return day;
 
         }
-        function getWeek(todayNum){
-            let dayArr =['Воскресенье','Понедельник','Вторник','Среда', 'Четверг','Пятница','Cуббота'];
+
+        function getWeek(todayNum) {
+            let dayArr = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Cуббота'];
             return dayArr[todayNum];
         }
 
-        function updateClock(){
-            let today = new Date();
-            let nowYear = today.getFullYear();
-            let ny = new Date('01 january '+(nowYear+1));
-            let timer = getTimeRemaining();
-            if(timer.hours===0 && timer.minutes===0 && timer.seconds===0){
-                textCon.style.color = 'red';
+        function hello(hours) {
+            let strHello;
+            if (hours >= 0 && hours < 4) {
+                strHello = 'Доброй ночи';
+            } else if (hours > 3 && hours < 12) {
+                strHello = 'Доброе утро';
+            } else if (hours > 11 && hours < 16) {
+                strHello = 'Добрый день';
+            } else if (hours > 15 && hours < 24) {
+                strHello = 'Добрый  вечер';
             }
-            /*timerHours.textContent = beautyNum(timer.hours);
-            timerMinutes.textContent = beautyNum(timer.minutes);
-            timerSeconds.textContent = beautyNum(timer.seconds); */
-            //textCon.textContent =''+beautyNum(timer.hours)+beautyNum(timer.minutes)+beautyNum(timer.seconds);
-            textCon.innerText ='Hello';
-
-
-            let idInterval;
-            if(timer.timeRemaining > 0) {
-                idInterval = setInterval(updateClock,1000);
-            }else {
-                clearInterval(idInterval);
-            }
+            return strHello;
         }
 
-        function beautyNum(num){
-            let numStr;
-            if(num<10){
-                numStr ='0'+num;
-            }else numStr =num;
-            return numStr;
+        function updateClock() {
+            let today = new Date(),
+                ny = new Date('01 january ' + (today.getFullYear() + 1));
+            textCon.innerHTML = `${hello(today.getHours())}<br>
+                       Сегодня: ${getWeek(today.getDay())}<br>
+                       Текущее время: ${today.toLocaleTimeString('EN')}<br>
+                        До нового года осталось дней: ${getCountDay(ny, today)} дней`;
+            clearTimeout(idTimeout);
         }
+
         updateClock();
-
-
-
     }
 
-    //  countTimer('31 july 2019');
-  //  setInterval(countTimer,1000,'01 july 2019');
-    setInterval(countTimer,1000,'01 july 2019');
+    let idTimeout = setTimeout(countTimer, 10);
 });
